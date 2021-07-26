525cc7e38971e0b7de7579966902a474563f49a1
Astha1804 <raiastha18042001@gmail.com>
2021-07-26 20:34:57 +0530

    Week 2

--- Week1/jumpSearch.cpp
+++ Week1/jumpSearch.cpp
@@ -1,28 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
-int binarySearch(int arr[],int n,int key)
+void binarySearch(int arr[],int n,int key)
 {
     
     int j = sqrt(n);
     int k = 0;
+    int cmp=0;
     while (arr[min(j, n)-1] < key)
     {
         k = j;
         j += sqrt(n);
-        if (k >= n)
-            return -1;
+        cmp++;
     }
  
     while (arr[k] < key)
     {
         k++;
-        if (k == min(j, n))
-            return -1;
+        cmp++;
     }
     if (arr[k] == key)
-        return k;
- 
-    return -1;
+        cout<<"Present "<<cmp<<"\n";
+    else{
+        cout<<"Not Present "<<cmp<<"\n";
+    }
 }
 int main()
 {   
@@ -38,10 +38,6 @@ int main()
         }
         int key,index;
         cin>>key;
-        index=binarySearch(arr,n,key);
-        if(index==-1)
-            cout<<"Not Present"<<" "<<index+1<<"\n";
-        else
-            cout<<"Present"<<" "<<index+1<<"\n";
+        binarySearch(arr,n,key);
     }
 }
